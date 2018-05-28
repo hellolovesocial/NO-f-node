9644c10290736840ed06101fdb83f76e848a5466
hellolovesocial <taylor.minako375@gmail.com>
2018-05-28 21:24:25 +0900

    Fixed routing

--- config/router.js
+++ config/router.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var app = express.Router();
 
-console.log('----arrived router.js-----')
+console.log('----arrived router.js-----');
 
-app.use(function (req, res, next) {
-  console.log('----books----')
+app.use('/', function (req, res, next) {
+  console.log('----flower----');
+  res.send('welcome to flower');
   next();
 });
 
-app.get('/:id', function (req, res, next) {
-  console.log('----books/:id-----')
+app.use('/users', function (req, res, next) {
+  console.log('----users----');
+  res.send('welcome to users page');
+  next();
+});
+
+app.get('/users/:id', function (req, res, next) {
+  console.log('----users/:id-----');
+  res.send('welcome to your page');
   next();
 });
 
