4e0054a49a497ef1d4291640c2067dbcde8974e5
hellolovesocial <taylor.minako375@gmail.com>
2018-05-30 15:40:53 +0900

    Added DB

--- config/router.js
+++ config/router.js
@@ -6,19 +6,19 @@ console.log('----arrived router.js-----');
 app.use('/', function (req, res, next) {
   console.log('----flower----');
   res.send('welcome to flower');
-  next();
+  rse.end();
 });
 
 app.use('/users', function (req, res, next) {
   console.log('----users----');
   res.send('welcome to users page');
-  next();
+  rse.end();
 });
 
 app.get('/users/:id', function (req, res, next) {
   console.log('----users/:id-----');
   res.send('welcome to your page');
-  next();
+  rse.end();
 });
 
 module.exports = app;
